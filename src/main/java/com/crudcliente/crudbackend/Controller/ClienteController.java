748b9b54dd4bccef6bbfe4194be1519639383878
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-29 01:53:53 -0300

    Definindo anotações do Cliente

--- src/main/java/com/crudcliente/crudbackend/Controller/ClienteController.java
+++ src/main/java/com/crudcliente/crudbackend/Controller/ClienteController.java
@@ -4,11 +4,14 @@ import com.crudcliente.crudbackend.model.Cliente;
 import com.crudcliente.crudbackend.repository.ClienteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/")
 public class ClienteController {
@@ -19,5 +22,10 @@ public class ClienteController {
   @PostMapping("/CadastroCliente")
   public Cliente addCliente(@RequestBody Cliente cliente) {
     return clienteRepository.save(cliente);
+  }
+
+  @GetMapping("/teste")
+  public String teste() {
+    return "Teste";
   }
 }
--- src/main/java/com/crudcliente/crudbackend/model/Cliente.java
+++ src/main/java/com/crudcliente/crudbackend/model/Cliente.java
@@ -1,32 +1,87 @@
 package com.crudcliente.crudbackend.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
+// import lombok.Data;
 
-@Data
-@NoArgsCOnstructor
-@AllArgsConstructor
+// @Data
+// @NoArgsCOnstructor
+// @AllArgsConstructor
 @Entity
-@Table
+@Table(name="cliente")
 public class Cliente {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", unique = true, nullable = false)
   private long id;
 
+  @Column(name = "nome")
   private String nome;
 
+  @Column(name = "cpf")
   private String cpf;
 
+  @Column(name = "telefone")
   private String telefone;
 
+  @Column(name = "observacao")
   private String observacao;
 
+  @Column(name = "endereco")
   private String endereco;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getObservacao() {
+    return observacao;
+  }
+
+  public void setObservacao(String observacao) {
+    this.observacao = observacao;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
   
 }
